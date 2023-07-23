e9db2bb3491ef13d4f14a96a45472fe2c42d9bff
Arash <arashkosariyeganeh@gmail.com>
2023-07-23 11:37:49 +0330

    Add ".env"

--- src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "http://api.weatherapi.com/v1";
-export const API_TOKEN = process.env.API_TOKEN || "<API_TOKEN>";
--- src/module/weather.js
+++ src/module/weather.js
@@ -1,8 +1,8 @@
-import { API_TOKEN, API_URL } from "../config";
+const { REACT_APP_API_URL, REACT_APP_API_TOKEN } = process.env;
 
 export async function searchCity(query) {
   const API_ROUTE = "/search.json";
-  const url = `${API_URL}${API_ROUTE}?key=${API_TOKEN}&q=${query}`;
+  const url = `${REACT_APP_API_URL}${API_ROUTE}?key=${REACT_APP_API_TOKEN}&q=${query}`;
 
   try {
     const response = await fetch(url);
@@ -16,7 +16,7 @@ export async function searchCity(query) {
 
 export async function getCurrent(city) {
   const API_ROUTE = "/current.json";
-  let url = `${API_URL}${API_ROUTE}?key=${API_TOKEN}&q=${city}`;
+  let url = `${REACT_APP_API_URL}${API_ROUTE}?key=${REACT_APP_API_TOKEN}&q=${city}`;
 
   try {
     const response = await fetch(url);
