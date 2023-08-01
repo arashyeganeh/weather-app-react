d1259ebda3010b4fc004dd7e8f45e347c7a45cb2
Arash <arashkosariyeganeh@gmail.com>
2023-08-01 16:47:05 +0330

    Use "axios" instead of "fetch"

--- src/module/weather.js
+++ src/module/weather.js
@@ -1,29 +1,39 @@
+import axios from "axios";
 const { REACT_APP_API_URL, REACT_APP_API_TOKEN } = process.env;
 
 export async function searchCity(query) {
   const API_ROUTE = "/search.json";
-  const url = `${REACT_APP_API_URL}${API_ROUTE}?key=${REACT_APP_API_TOKEN}&q=${query}`;
+  const url = `${REACT_APP_API_URL}${API_ROUTE}`;
 
   try {
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
+    const response = await axios.get(url, {
+      params: {
+        key: REACT_APP_API_TOKEN,
+        q: query
+      }
+    })
+    return response.data;
+
   } catch (error) {
-    console.log(error);
-    return null;
+    console.log(error)
+    return false;
   }
 }
 
 export async function getCurrent(city) {
   const API_ROUTE = "/current.json";
-  let url = `${REACT_APP_API_URL}${API_ROUTE}?key=${REACT_APP_API_TOKEN}&q=${city}`;
+  let url = `${REACT_APP_API_URL}${API_ROUTE}`;
 
   try {
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
+    const response = await axios.get(url, {
+      params: {
+        key: REACT_APP_API_TOKEN,
+        q: city
+      }
+    })
+    return response.data;
   } catch (error) {
     console.log(error);
-    return null;
+    return false;
   }
 }
